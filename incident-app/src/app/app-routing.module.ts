c95242adbaa2c240a06c18c8039d9e8343aa0211
Néstor Romero León <romero.nestor@gmail.com>
2021-03-30 00:37:49 -0400

    Basic navigation and components for users and auth functionality

--- incident-app/src/app/app-routing.module.ts
+++ incident-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { IncidentFormComponent } from './incidents/incident-form/incident-form.component';
+import { IncidentListComponent } from './incidents/incident-list/incident-list.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'incidents/create', component : IncidentFormComponent},
+  {path: 'incidents', component : IncidentListComponent},
+  {path: 'users/register', component : UserFormComponent},
+  {path: 'login', component : LoginComponent},
+  {path: 'logout', component : HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- incident-app/src/app/app.module.ts
+++ incident-app/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { IncidentListComponent } from './incidents/incident-list/incident-list.component';
+import { IncidentFormComponent } from './incidents/incident-form/incident-form.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IncidentListComponent,
+    IncidentFormComponent,
+    HomeComponent,
+    LoginComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
